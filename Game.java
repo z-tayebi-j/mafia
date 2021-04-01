4bf69f06a57ff34f54aad37a3e6d84fa9702860b
Zahra Tayebi <s.z.tayebi.j@gmail.com>
2021-04-01 07:43:41 -0700

    start_game and get_game_state methods were added.

--- Game.java
+++ Game.java
@@ -2,45 +2,53 @@
 import java.util.Scanner;
 
 public class Game {
+    public static int numOfPlayers = 0;
     public static int numOfMafias = 0;
     public static int numOfVillagers = 0;
-    mafia[] mafias = new mafia[50];
-    villager[] villagers = new villager[50];
+    public static Player[] allplayers = new Player[100];
 
-    public static void assign_role(String[] players, String name, String role) {
-        for (int i = 0; i < players.length; ++i)
-            if (name.equals(players[i])) {
+    public static void assign_role(String[] playersnames, String name, String role) {
+        for (int i = 1; i < playersnames.length; ++i)
+            if (name.equals(playersnames[i])) {
                 switch (role) {
                     case "Joker":
-                        Joker j = new Joker(name);
+                        allplayers[numOfPlayers] = new Joker(name);
+                        ++numOfPlayers;
                         break;
                     case "mafia":
-                        mafia m = new mafia(name);
+                        allplayers[numOfPlayers] = new mafia(name);
                         ++numOfMafias;
+                        ++numOfPlayers;
                         break;
                     case "godfather":
-                        godfather g = new godfather(name);
+                        allplayers[numOfPlayers] = new godfather(name);
                         ++numOfMafias;
+                        ++numOfPlayers;
                         break;
                     case "silencer":
-                        silencer s = new silencer(name);
+                        allplayers[numOfPlayers] = new silencer(name);
                         ++numOfMafias;
+                        ++numOfPlayers;
                         break;
                     case "villager":
-                        villager v = new villager(name);
+                        allplayers[numOfPlayers] = new villager(name);
                         ++numOfVillagers;
+                        ++numOfPlayers;
                         break;
                     case "doctor":
-                        doctor d = new doctor(name);
+                        allplayers[numOfPlayers] = new doctor(name);
                         ++numOfVillagers;
+                        ++numOfPlayers;
                         break;
                     case "detective":
-                        detective de = new detective(name);
+                        allplayers[numOfPlayers] = new detective(name);
                         ++numOfVillagers;
+                        ++numOfPlayers;
                         break;
                     case "bulletproof":
-                        bulletproof b = new bulletproof(name);
+                        allplayers[numOfPlayers] = new bulletproof(name);
                         ++numOfVillagers;
+                        ++numOfPlayers;
                         break;
                     default:
                         System.out.println("role not found");
@@ -50,16 +58,28 @@ public class Game {
         System.out.println("user not found");
     }
 
+    public static void start_game() {
+        for (int i = 0; i < numOfPlayers; ++i)
+            System.out.println(allplayers[i].name + ": " + allplayers[i].role);
+        System.out.println("\nReadi? Set! Go.");
+    }
+
+    public static void get_game_state() {
+        System.out.println("Mafia = " + numOfMafias);
+        System.out.println("Villager = " + numOfVillagers);
+    }
+
     public static void main(String[] args) {
         String[] playersNames = null;
         boolean creat_game = false;
+        boolean start_game = false;
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        while (scanner.hasNext()) {
             String s = scanner.next();
             switch (s) {
                 case "creat_game":
                     creat_game = true;
-                    String names = scanner.next();
+                    String names = scanner.nextLine();
                     playersNames = names.split(" ");
                     break;
                 case "assign_role":
@@ -72,12 +92,23 @@ public class Game {
                     }
                     break;
                 case "start_game":
+                    if (creat_game == false)
+                        System.out.println("no game created");
+                    else if (start_game)
+                        System.out.println("game has already started");
+                    else if (numOfPlayers != playersNames.length - 1)
+                        System.out.println("one or more players do not have a role");
+                    else {
+                        start_game = true;
+                        start_game();
+                    }
                     break;
                 case "end_vote":
                     break;
                 case "end_night":
                     break;
                 case "get_game_state":
+                    get_game_state();
                     break;
             }
 
@@ -89,6 +120,7 @@ class Player {
     public String name;
     public int NumOfDayVotes;
     public boolean isAlive = true;
+    public String role;
 
     public Player(String name) {
         this.name = name;
@@ -98,18 +130,21 @@ class Player {
 class mafia extends Player {
     public mafia(String name) {
         super(name);
+        role = "mafia";
     }
 }
 
 class godfather extends mafia {
     public godfather(String name) {
         super(name);
+        role = "godfather";
     }
 }
 
 class silencer extends mafia {
     public silencer(String name) {
         super(name);
+        role = "silencer";
     }
 }
 
@@ -118,24 +153,28 @@ class villager extends Player {
 
     public villager(String name) {
         super(name);
+        role = "villager";
     }
 }
 
 class doctor extends villager {
     public doctor(String name) {
         super(name);
+        role = "doctor";
     }
 }
 
 class detective extends villager {
     public detective(String name) {
         super(name);
+        role = "detective";
     }
 }
 
 class bulletproof extends villager {
     public bulletproof(String name) {
         super(name);
+        role = "bulletproof";
     }
 }
 
@@ -144,5 +183,6 @@ class Joker extends Player {
 
     public Joker(String name) {
         super(name);
+        role = "Joker";
     }
 }
